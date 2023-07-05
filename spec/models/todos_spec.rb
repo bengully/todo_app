23897b6ca257984f869e6cb9391625ea5ff0d50f
Ben Gully <gullybenjamin@yahoo.com>
2023-07-05 11:39:18 -0400

    Tests invalid case in model

--- spec/models/todos_spec.rb
+++ spec/models/todos_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Todo, type: :model do
-    let(:todo) { Todo.new(task: 'make test') } 
+RSpec.describe Todo, type: :model do 
+    context 'when a task is specified' do 
+        let(:todo) { Todo.new(task: 'make test') }
 
-    it 'is valid with a task' do
-        expect(todo).to be_valid
+        it 'is valid' do
+            expect(todo).to be_valid
+        end
+
+        it 'defaults to open status when created' do
+            todo.save
+            expect(todo.status).to eq 'open'
+        end
     end
-    
-    it 'defaults to open status when created' do
-        todo.save
-        expect(todo.status).to eq 'open'
+
+    context 'when no task is specified' do
+        it 'is invalid' do
+            expect(Todo.new).not_to be_valid
+        end
     end
 end
