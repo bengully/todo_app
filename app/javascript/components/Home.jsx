0573103cdc14a8e6a102cea48c7d72f78fedac2c
Ben Gully <gullybenjamin@yahoo.com>
2023-07-05 11:22:48 -0400

    Add basic list

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
-export default () => (
-  <div>
-   <h1>To Do List</h1>
-  </div>
-);
+const Home = () => {
+    const [todos, setTodos] = useState();
+
+    useEffect(() => {
+        const url = "/api/todos";
+        fetch(url).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Something bad happened...");
+        })
+        .then((response) => { setTodos(response); }).then(console.log(todos))
+    }, []);
+
+    let todo_list = null;
+    if(todos){
+        todo_list = todos.map((todo, index) => (
+            <div key={index}>
+                <h2>{todo.task}</h2>
+                <p>{todo.description}</p>
+            </div>
+        ))
+    }
+    
+    return(
+        <>
+            {todo_list}
+        </>
+    )
+}
+
+export default Home;
